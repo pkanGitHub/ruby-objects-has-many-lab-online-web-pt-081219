ca72095c88802802f9a89d9f1aa6ed4a460c7517
Phyllis Kan <pyk9q7@gmail.com>
2019-09-30 02:26:56 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,20 +1,20 @@
-class Author 
-  attr_accessor :name 
+class Author
+  attr_accessor :name
 
   @@all = []
 
   def initialize(name)
-    @name = name 
-    @@all << self 
+    @name = name
+    @@all << self
   end
 
-  def add_post(post) 
-    post.author = self 
+  def add_post(post)
+    post.author = self
   end
 
   def add_post_by_title(title)
     post = Post.new(title)
     post.author == self
   end
-  
+
 end

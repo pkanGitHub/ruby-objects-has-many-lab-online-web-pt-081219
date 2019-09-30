84a954e71d96fb754984d047ae9ef4d3babd7af7
Phyllis Kan <pyk9q7@gmail.com>
2019-09-30 02:09:42 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,18 +1,19 @@
 class Artist
   attr_accessor :name
 
-  @@all = []
-
   def initialize(name)
     @name = name
-    save
   end
 
-  def save
-    @@all << self
+  def add_song(song)
+    song.artist = self
   end
 
-  def self.all
-    @@all
+  def songs
+    Song.all.select {|song| song.artist == self}
+  end
+
+  def add_song_by_name(name)
+    song = Song.new(name)
   end
 end

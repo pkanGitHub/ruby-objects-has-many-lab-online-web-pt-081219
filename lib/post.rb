f3d5818e27c46d692cc1fccbf521a0b893cde464
Phyllis Kan <pyk9q7@gmail.com>
2019-09-30 02:32:05 +0000

    Automatically backed up by Learn

---
+++ lib/post.rb
@@ -0,0 +1,19 @@
+class Post 
+  attr_accessor :title, :author 
+
+  @@all = []
+
+  def initialize(title)
+    @title = title 
+    @@all << self
+  end
+
+  def author_name 
+    author.name if author
+  end
+
+  def self.all 
+    @@all
+  end
+  
+end
